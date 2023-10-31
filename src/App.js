7366d279f6037662be309adaaa2b2a2709cddcfb
Raj Prakash Maurya <raj232033@gmail.com>
2023-10-31 16:59:42 +0530

    register completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import "./Styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,9 +7,15 @@ import "./style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Add from "../img/image_icon_153794.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   // console.log(e.target);
+  //   const displayName = e.target[0].value;
+  //   const email = e.target[1].value;
+  //   const password = e.target[2].value;
+  //   const file = e.target[3].files[0];
+
+  //   try {
+  //     const res = await createUserWithEmailAndPassword(auth, email, password);
+
+  //     const storageRef = ref(storage, displayName);
+
+  //     // const uploadTask = uploadBytesResumable(storageRef, file);
+
+  //     uploadTask.on(
+  //       (error) => {
+  //         // Handle unsuccessful uploads
+  //         setError(true);
+  //       },
+  //       () => {
+  //         getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+  //           await updateProfile(res.user, {
+  //             displayName,
+  //             photoURL: downloadURL,
+  //           });
+  //           await setDoc(doc(db, "users", res.user.uid), {
+  //             uid: res.user.uid,
+  //             displayName,
+  //             email,
+  //             photoURL: downloadURL,
+  //           });
+  //           await setDoc(doc(db, "userChats", res.user.uid), {});
+  //           navigate("/");
+  //         });
+  //       }
+  //     );
+  //   } catch (error) {
+  //     setError(true);
+  //   }
+  // };
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
+      setErr(true);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Raj Chat</span>
         <span className="title">Register</span>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input className="input" type="text" placeholder="Enter name" />
           <input className="input" type="email" placeholder="Enter email" />
           <input
@@ -21,9 +119,15 @@ function Register() {
             <span style={{ paddingLeft: "5px" }}>Add an avatar</span>
           </label>
 
-          <button className="button">Sign Up</button>
+          <button disabled={loading} className="button">
+            Sign Up
+          </button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>SomeThing went wrong</span>}
         </form>
-        <p className="p">You do have Account ? Login</p>
+        <p className="p">
+          You do have Account ? <Link to="/login">Login</Link>
+        </p>
       </div>
     </div>
   );
